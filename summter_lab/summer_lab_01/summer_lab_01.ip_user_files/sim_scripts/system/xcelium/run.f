935635741f943fdd90cd4376bc5124c5646d30fd
Yunpeng Wang <ypwang0905@gmail.com>
2022-07-09 17:26:32 +0800

    day 01

---
+++ summter_lab/summer_lab_01/summer_lab_01.ip_user_files/sim_scripts/system/xcelium/run.f
@@ -0,0 +1,28 @@
+-makelib xcelium_lib/xilinx_vip -sv \
+  "/home/ypwang/software/Vivado/2021.1/data/xilinx_vip/hdl/axi4stream_vip_axi4streampc.sv" \
+  "/home/ypwang/software/Vivado/2021.1/data/xilinx_vip/hdl/axi_vip_axi4pc.sv" \
+  "/home/ypwang/software/Vivado/2021.1/data/xilinx_vip/hdl/xil_common_vip_pkg.sv" \
+  "/home/ypwang/software/Vivado/2021.1/data/xilinx_vip/hdl/axi4stream_vip_pkg.sv" \
+  "/home/ypwang/software/Vivado/2021.1/data/xilinx_vip/hdl/axi_vip_pkg.sv" \
+  "/home/ypwang/software/Vivado/2021.1/data/xilinx_vip/hdl/axi4stream_vip_if.sv" \
+  "/home/ypwang/software/Vivado/2021.1/data/xilinx_vip/hdl/axi_vip_if.sv" \
+  "/home/ypwang/software/Vivado/2021.1/data/xilinx_vip/hdl/clk_vip_if.sv" \
+  "/home/ypwang/software/Vivado/2021.1/data/xilinx_vip/hdl/rst_vip_if.sv" \
+-endlib
+-makelib xcelium_lib/axi_infrastructure_v1_1_0 \
+  "../../../../summer_lab_01.gen/sources_1/bd/system/ipshared/ec67/hdl/axi_infrastructure_v1_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/axi_vip_v1_1_10 -sv \
+  "../../../../summer_lab_01.gen/sources_1/bd/system/ipshared/0980/hdl/axi_vip_v1_1_vl_rfs.sv" \
+-endlib
+-makelib xcelium_lib/processing_system7_vip_v1_0_12 -sv \
+  "../../../../summer_lab_01.gen/sources_1/bd/system/ipshared/f42d/hdl/processing_system7_vip_v1_0_vl_rfs.sv" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../bd/system/ip/system_processing_system7_0_0/sim/system_processing_system7_0_0.v" \
+  "../../../bd/system/sim/system.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
